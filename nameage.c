0a95ef19e2ca1c19379d2be95471317e33f6c035
K Arya <aryapandey723@gmail.com>
2025-09-08 14:42:10 +0530

    Add files via upload

---
+++ nameage.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+    int age;
+
+    printf("Enter your name: ");
+    scanf("%s", name);   // allows spaces in name
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    return 0;
+}
---
+++ newline.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name: ARYA\n");
+    printf("Course: BTech CSE\n");
+    printf("College: UPES Dehradun\n");
+    printf("PIN Code: 248007\n");
+    return 0;
+}
