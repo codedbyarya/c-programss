9416707647d72b8c6c41b63245184ab667a4a140
K Arya <aryapandey723@gmail.com>
2025-09-22 14:41:35 +0530

    exp4.3.c

--- exp4.3.c
+++ exp4.3.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int x = 10;  // Declared in main
+    int x = 10;  
 
     {
-        int y = 20;  // Declared inside block
+        int y = 20;  
         printf("Inside block: x = %d, y = %d\n", x, y);
     }
 
-    // printf("Outside block: y = %d\n", y); // ❌ Error: y not accessible here
     printf("Outside block: x = %d\n", x);
 
     return 0;
 }
+
